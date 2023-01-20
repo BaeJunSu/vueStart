f71589bb5936d65a3172ffccd8a207ba20177e56
DESKTOP-V0K9MU2\user <bjs2417@inswave.com>
2023-01-20 05:33:08 +0000

    first commit3

--- src/Main.vue
+++ src/Main.vue
@@ -1,12 +1,24 @@
 <template>
   <!-- 헤더-->
-  <div>
+  <div class="selection_container">
     <!-- 메인 3개 column -->
+    <Selection />
   </div>
 </template>
 
 <script>
-export default {}
+import Selection from './components/Selection.vue'
+export default {
+  computed: {
+    Selection,
+  },
+}
 </script>
 
-<style></style>
+<style>
+.selection_container {
+  display: flex;
+  flex-direction: row;
+  border: solid 1px gray;
+}
+</style>
---
+++ src/components/Selection.vue
@@ -0,0 +1,14 @@
+<template>
+  </template>
+
+  <script>
+    export default {}
+  </script>
+
+  <style>
+    .container {
+      flex: 1;
+      width: 200px;
+    }
+  </style>
+</template>
